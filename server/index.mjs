7c5ee0e31fc8ad9e78ef9783e6b1a1ce456cfd36
Mykyta Yanushevskyi <raz0r69jpeg@gmail.com>
2024-05-12 13:40:25 +0300

    Updates

---
+++ server/index.mjs
@@ -0,0 +1,12 @@
+import process from 'node:process';globalThis._importMeta_={url:import.meta.url,env:process.env};import 'node:http';
+import 'node:https';
+export { B as default } from './chunks/runtime.mjs';
+import 'lru-cache';
+import 'fs';
+import 'path';
+import 'cheerio/lib/slim';
+import 'node:crypto';
+import 'node:fs';
+import 'node:url';
+import 'xss';
+//# sourceMappingURL=index.mjs.map
